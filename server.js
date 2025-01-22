212a974514619a8fab00c30156aa168714d9dd69
Fahmi <fahmiardiansyah959@students.unnes.ac.id>
2025-01-22 11:13:41 +0700

    backend cctv fahmi

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public")); // Folder untuk file HTML
+
+// Ketika ada client yang terhubung
+io.on("connection", (socket) => {
+  console.log(User connected: ${socket.id});
+
+  // Terima video stream dari Android client
+  socket.on("videoStream", (data) => {
+    // Kirim stream ke semua client yang terhubung (browser)
+    socket.broadcast.emit("receiveStream", { id: socket.id, data });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(User disconnected: ${socket.id});
+  });
+});
+
+// Jalankan server di port 3000
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
