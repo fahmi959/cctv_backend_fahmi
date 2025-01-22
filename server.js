22c84e5dcc89a82120a5dd7baaa30de424663f0f
Fahmi <fahmiardiansyah959@students.unnes.ac.id>
2025-01-22 12:13:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+// server.js (Backend: WebSocket signaling)
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 3000 });
 
-// Serve static files (you can place your HTML file here)
-app.use(express.static("public"));
+wss.on('connection', function connection(ws) {
+    console.log("Client connected");
 
-io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`);
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        // Broadcast message to all connected clients (signaling data)
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
 
-  socket.on("videoStream", (data) => {
-    console.log("Received video stream data from client:", data);
-
-    // Broadcast the binary stream data to all connected clients
-    socket.broadcast.emit("receiveStream", { id: socket.id, data: data });
-  });
-
-  socket.on("disconnect", () => {
-    console.log(`User disconnected: ${socket.id}`);
-  });
-});
-
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+    ws.on('close', () => {
+        console.log("Client disconnected");
+    });
 });
